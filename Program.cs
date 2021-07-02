b793f7e6151ed28af0bfa24aacbd0fb05eed83f3
InsanePhin <insanephin@blacklight.tk>
2021-07-03 03:38:05 +0900

    study string upper lower

--- Program.cs
+++ Program.cs
@@ -36,6 +36,9 @@ namespace dotnet
             Console.WriteLine(sayHello);
             sayHello = sayHello.Replace("Hello", "Greetings");
             Console.WriteLine(sayHello);
+
+            Console.WriteLine(sayHello.ToUpper());
+            Console.WriteLine(sayHello.ToLower());      
         }
     }
 }
