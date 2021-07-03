2e30f5f1efd7ba7e62444805515e2a09f99ea99e
InsanePhin <insanephin@blacklight.tk>
2021-07-04 02:55:21 +0900

    Create Program.cs

---
+++ int/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
