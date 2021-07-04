81b74da1a9e07ee941ed6b2bc24fbbe24c7a9c64
InsanePhin <insanephin@blacklight.tk>
2021-07-04 22:36:46 +0900

    study calculate

--- int/Program.cs
+++ int/Program.cs
@@ -6,7 +6,11 @@ namespace ints
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 1;
+            int b = 2;
+            int c = a + b;
+            
+            Console.WriteLine(c);
         }
     }
 }
